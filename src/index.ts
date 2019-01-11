040ce8725bc477c321d737806c064a5bfbe0990e
nullabletypo <masahirokato.dev@gmail.com>
2019-01-11 14:39:54 +0900

    chore: add prettier

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,17 @@
 // @ts-ignore
 if (module.hot) {
   // @ts-ignore
-  module.hot.accept([/* dependencies */], status => {
-    // dependenciesがupdateされたときのハンドラ
-  })
+  module.hot.accept(
+    [
+      /* dependencies */
+    ],
+    // @ts-ignore
+    status => {
+      // dependenciesがupdateされたときのハンドラ
+    },
+  )
   // @ts-ignore
   module.hot.dispose(data => {
     // 現在のmodule(index.ts)がupdateされる場合のハンドラ
   })
 }
-
